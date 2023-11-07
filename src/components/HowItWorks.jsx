721e5e317c1a82cf495cc5a71c8c9d7d12806718
KrownWealth <abdulramonadeola36@gmail.com>
2023-11-07 16:25:53 +0100

    Added styling to how it works, implement next-font for optimization, started another section names Village market

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -2,50 +2,53 @@ import Image from "next/image";
 
 const HowItWorks = () => {
   return(
-    <section className="px-16 py-12 bg-blue-100">
-       <div className="grid grid-cols-2 gap-y-12 lg:hidden ">
-       <div>
+    <section className=" px-8 lg:px-16 py-20 bg-white pt-20">
+      <h1 className="pb-20">How it Works </h1>
+        {/*Display on mobile*/}
+       <div className="grid grid-cols-3 gap-y-16 lg:hidden gap-x-4">
+       <div className="col-span-1">
           <Image src="/Assets/Images/search trans.svg" alt="search" width={100} height={100} />
         </div>
-        <div>
-         <h1>Search</h1>
-         <h2>Search for restaurants, cuisines with the automatic advance location detection search option.</h2>
+        <div className="col-span-2">
+        <h2>Search</h2>
+          <h3>Search for restaurants, cuisines with the automatic advance location detection search option.</h3>
         </div>
-        <div>
+        <div className="col-span-1">
           <Image src="/Assets/Images/Order order&pay.svg" alt="search" width={100} height={100} />
         </div>
-        <div>
-         <h1>Search</h1>
-         <h2>Search for restaurants, cuisines with the automatic advance location detection search option.</h2>
+        <div className="col-span-2">
+          <h2>Order & Pay </h2>
+          <h3>Order delicious meal & Pay with Card or Bank transfer through our secure Payment Gateway</h3>
         </div>
-        <div>
+        <div className="col-span-1">
           <Image src="/Assets/Images/serve.svg" alt="search" width={100} height={100} />
         </div>
-        <div>
-         <h1>Search</h1>
-         <h2>Search for restaurants, cuisines with the automatic advance location detection search option.</h2>
+        <div className="col-span-2">
+        <h2>Instant Order Fulfilment</h2>
+          <h3>Have your meal served Hot & Fast as soon as you bus stop over at the restaurants. Enjoy your meal!</h3>
         </div>
         </div>
-        <div className="lg:grid gap-4 md:grid-cols-3 hidden ">
+        {/*Display on desktop*/}
+        <div className="lg:grid gap-4 md:grid-cols-3 hidden text-center">
         {/* First item */}
-        <div className="text-center ">
-          <Image src="/Assets/Images/search.svg" alt="search" width={100} height={100} className="m-auto" />
-          <h1>Search</h1>
-          <h2>Search for restaurants, cuisines with the automatic advance location detection search option.</h2>
+        <div>
+          <Image src="/Assets/Images/search trans.svg" alt="search for fast-food, cuisines, and dishes " width={100} height={100} className="m-auto" />
+          <h2>Search</h2>
+          <h3>Search for restaurants, cuisines with the automatic advance location detection search option.</h3>
         </div>
 
         {/* Second item */}
-        <div className="text-center">
-          <Image src="/Assets/Images/search.svg" alt="search" width={100} height={100} className="m-auto"  />
-          <h1>Search</h1>
-          <h2>Search for restaurants, cuisines with the automatic advance location detection search option.</h2>
+        <div>
+          <Image src="/Assets/Images/Order order&pay.svg" alt="order and make payment" width={100} height={100} className="m-auto"  />
+          <h2>Order & Pay </h2>
+          <h3>Order delicious meal & Pay with Card or Bank transfer through our secure Payment Gateway</h3>
         </div>
 
         {/* Third item */}
-        <div className="text-center">
-          <Image src="/Assets/Images/search.svg" alt="search" width={100} height={100} className="m-auto"  />
-          <h1>Search</h1>
-          <h2>Search for restaurants, cuisines with the automatic advance location detection search option.</h2>
+        <div>
+          <Image src="/Assets/Images/serve.svg" alt="get your food serve" width={100} height={100} className="m-auto"  />
+          <h2>Instant Order Fulfilment</h2>
+          <h3>Have your meal served Hot & Fast as soon as you bus stop over at the restaurants. Enjoy your meal!</h3>
         </div>
       </div>
       
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css';
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
+import { Raleway } from 'next/font/google';
+
+const raleway = Raleway({ 
+  subsets: ['latin'],
+  weight: ['200', '300', '400','500', '600', '700', '800', '900'],
+   });
+
 
 export default function App({ Component, pageProps }) {
   return (
