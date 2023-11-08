fae1b45f3de440ce6848a6beb0facf256ef9c580
KrownWealth <abdulramonadeola36@gmail.com>
2023-11-08 22:56:01 +0100

    improved village market layouts, now working on the explore market section

--- src/components/ExploreMarket.jsx
+++ src/components/ExploreMarket.jsx
@@ -7,10 +7,12 @@ const ExploreMarket = () => {
     <section className={styles.villageSection}>
       <div className={styles.villageImg}>
      <div className={styles.exploreContainer}>
+      <div className={styles.textContainer}>
       <h1 className="text-white">Get grocery and convenience store essentials</h1>
       <h2 className="text-white">Grocery delivery, exactly how you want it.</h2>
       <h3 className="text-white">Shop from home and fill your cart with fresh produce, frozen entrees, deli delights, and more.</h3>
-      <button className={styles.longBtn}><Link href="">Explore Village Market</Link></button>
+      <button className="w-[220px]"><Link href="">Explore Village Market</Link></button>
+     </div>
      </div>
       </div>
     </section>
--- src/components/VillageMarket.jsx
+++ src/components/VillageMarket.jsx
@@ -4,42 +4,46 @@ import styles from "../styles/VillageMarket.module.css"
 
 const VillageMarket = () => {
   return(
-    <section className=" px-8 lg:px-16 py-20 bg-white pt-20 w-[100%]">
+    <>
+    <section className=" px-8 lg:px-16 pt-20 bg-white w-[100%]">
       {/* <h1 className="pb-20">Best Seller from Village Markets </h1> */}
      <div className="flex flex-col lg:flex-row lg:space-x-8 py-12">
      <div className="basis-full lg:basis-2/3 block lg:hidden">
       <Image src="/Assets/Images/banana on vendor table 1.jpg" alt="banana on vendor table" width={100} height={100}  className="w-[100%]"   />
       </div>
-      <div className="basis-full lg:basis-1/3 pt-12 lg:pt-12">
+      <div className="basis-full lg:basis-1/3 pt-12 lg:pt-12 lg:text-left space-y-6">
       <h1>Everything you crave, served.</h1>
       <h2>Your favorite local restaurants</h2>
 <h3>Get a slice of pizza or the whole pie delivered, or pick up house lo mein from the Chinese takeout spot youve been meaning to try.
       </h3>
-      <button className={styles.longBtn}><Link href="">Find Fast-Food</Link></button>
+      <button className="longBtn"><Link href="">Find Fast-Food</Link></button>
         </div>
-      <div className="basis-full lg:basis-2/3  hidden lg:block">
+      <div className="basis-full lg:basis-2/3  hidden lg:block mt-10">
       <Image src="/Assets/Images/banana on vendor table 1.jpg" alt="banana on vendor table" width={100} height={100}  className="w-[100%]" />
       </div>
       
 
      </div>
-     <div className="flex flex-col lg:flex-row lg:space-x-12 pv-12">
+     </section>
+     <section className=" px-8 lg:px-16 py-20  bg-[#f8f4d3]  w-[100%]">
+     <div className="flex flex-col lg:flex-row lg:space-x-12 ">
      <div className="basis-full lg:basis-2/3">
       <Image src="/Assets/Images/banana on vendor table 1.jpg" alt="banana on vendor table" width={100} height={100} 
       className="w-[100%]"/>
       </div>
-      <div className="basis-full lg:basis-1/3 pt-12 lg:pt-12">
+      <div className="basis-full lg:basis-1/3 pt-12 lg:pt-12 lg:text-left space-y-6">
       <h1>Everything you crave, served.</h1>
-      <h2>Your favorite local restaurants</h2>
+      <h2>Your favorite road-side local fast-food</h2>
 <h3>Get a slice of pizza or the whole pie delivered, or pick up house lo mein from the Chinese takeout spot you've been meaning to try.
       </h3>
-      <button className={styles.longBtn}><Link href="">Find Fast-Food</Link></button>
+      <button className="longBtn"><Link href="">Find Fast-Food</Link></button>
         </div>
      
       
 
      </div>
      </section>
+     </>
   )
 }
 
